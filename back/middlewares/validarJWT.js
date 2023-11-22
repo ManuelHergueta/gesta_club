6df8b6cc61745a20286624bc737beb4ff2ea2976
ManuelHergueta <manuelherguetaharo@gmail.com>
2023-11-22 20:55:54 +0100

    middlewares y validator en auth y usuario

---
+++ back/middlewares/validarJWT.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const {response, request} = require('express');
+
+const validarJWT = (req , res , next) => {
+
+    const token = req.header('x-token');
+
+    if (!token){
+        return res.status(401).json({'msg':'No hay token en la petición.'});
+    }
+
+    try {    
+        const {uemail} = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+        req.emailToken = uemail;
+
+        console.log(uemail);
+        console.log(token);
+        
+        next();
+        
+    }catch(error){
+        console.log(error);
+        res.status(401).json({'msg':'Token no válido.'});
+    }
+}
+
+module.exports = {
+    validarJWT
+}
--- back/routes/authRoutes.js
+++ back/routes/authRoutes.js
@@ -1,7 +1,16 @@
 const { Router } = require('express');
-const controlador = require('../controllers/authController');
 const router = Router();
+const { check } = require('express-validator');
 
-router.post('/login',controlador.login);
+const controlador = require('../controllers/authController');
+const { validarCampos } = require('../middlewares/validarMiddlewares');
+
+router.post('/login',
+    [
+        check('email', 'Introduzca un email valido').isEmail(),
+        check('password', 'El password es obligatorio').notEmpty(),
+        validarCampos
+    ], 
+    controlador.login);
 
 module.exports = router;
